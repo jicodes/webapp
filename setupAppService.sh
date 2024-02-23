adaf6414bf7c9bd4ea6b2df80fec5fa60386eff0
Ji <122512755+jicodes@users.noreply.github.com>
2024-02-23 14:15:42 -0800

    debug: added status check for webapp (#21)

--- setupAppService.sh
+++ setupAppService.sh
@@ -3,3 +3,9 @@
 # Configure systemd service to start on boot
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
+
+# Start the service
+sudo systemctl start webapp.service
+
+# Check the status of the service
+sudo systemctl status webapp.service
