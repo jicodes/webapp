6cc396bc1e4ac5e9b81f924c74cf8c79a71cef86
Ji <122512755+jicodes@users.noreply.github.com>
2024-02-24 12:56:37 -0800

    fix setup db auth (#28)

--- setupDB.sh
+++ setupDB.sh
@@ -24,11 +24,10 @@ sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 echo "listen_addresses = '*'" | sudo tee -a /var/lib/pgsql/data/postgresql.conf
 
 # Allow incoming connections to PostgreSQL from all hosts
-echo "host all all 0.0.0.0/0  md5" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
-echo "host all all ::0/0  md5" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
-
-# Allow local connections to PostgreSQL from the postgres user
-echo "local all all md5" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
+# Replace the id range and auth method in pg_hba.conf
+sudo sed -i -e 's/127.0.0.1\/32 *ident/0.0.0.0\/0 md5/' \
+            -e 's/::1\/128 *ident/::0\/0 md5/' \
+            /var/lib/pgsql/data/pg_hba.conf
 
 # Restart PostgreSQL for the changes to take effect
 echo "Starting PostgreSQL service..."
