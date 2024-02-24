fe6edc7286bf3a8652aa5bdf9e5fe65ce46eb163
Ji <122512755+jicodes@users.noreply.github.com>
2024-02-24 11:05:33 -0800

    fix setup db (#27)

--- setupDB.sh
+++ setupDB.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -e 
+set -e  # Exit on error
+set -o pipefail  # Exit on pipeline command failure
 
 # Install PostgreSQL with enabled module
 sudo dnf module list postgresql 
 sudo dnf module enable -y postgresql:16
 sudo dnf install -y postgresql-server 
 
-# Initialize PostgreSQL database
+echo "Initializing PostgreSQL database..."
 sudo postgresql-setup --initdb
 
 # Start PostgreSQL service
@@ -15,8 +16,6 @@ sudo systemctl start postgresql
 
 # Enable PostgreSQL service to start on boot
 sudo systemctl enable postgresql
-
-sudo -u postgres psql template1
 
 # Change the password for the PostgreSQL default user 'postgres'
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
@@ -26,9 +25,11 @@ echo "listen_addresses = '*'" | sudo tee -a /var/lib/pgsql/data/postgresql.conf
 
 # Allow incoming connections to PostgreSQL from all hosts
 echo "host all all 0.0.0.0/0  md5" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
+echo "host all all ::0/0  md5" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
 
 # Allow local connections to PostgreSQL from the postgres user
 echo "local all all md5" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
 
-# Reload PostgreSQL for the changes to take effect
-sudo systemctl reload postgresql
+# Restart PostgreSQL for the changes to take effect
+echo "Starting PostgreSQL service..."
+sudo systemctl restart postgresql
