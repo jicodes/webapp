cdfbc4cef6317c01eccff1e3ca5ace7b81c73614
Ji <122512755+jicodes@users.noreply.github.com>
2024-02-28 20:04:46 -0800

    remove debug logs (#39)

--- setupAppService.sh
+++ setupAppService.sh
@@ -3,9 +3,3 @@
 # Configure systemd service to start on boot
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
-
-sudo systemctl start webapp.service
-
-sudo systemctl status webapp.service
-# log output from the service
-journalctl -u webapp.service
