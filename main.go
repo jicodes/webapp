743691b9e2933eac6254bc4d2babb5996a334a0d
Ji <122512755+jicodes@users.noreply.github.com>
2024-03-29 12:08:57 -0700

    fix route for email verification (#65)

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func setupRouter() *gin.Engine {
 
 	r.Any("/healthz", middlewares.CheckRequestMethod(), middlewares.CheckPayload(), handlers.CheckHealthz)
 	r.POST("/v1/user", handlers.CreateUser)
-	r.POST("/v1/user/verify/:token", handlers.VerifyEmail)
+	r.GET("/v1/user/verify/", handlers.VerifyEmail)
 
 	userGroup := r.Group("/v1/user")
 	userGroup.Use(middlewares.BasicAuth())  
